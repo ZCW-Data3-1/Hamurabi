793241be01248a1fcdef44e6f4b253d154c8d6f1
Nick Nilson <nilson.nicholas@gmail.com>
2022-07-09 10:33:58 -0400

    update before making some changes to class variables

--- hamurabiv2.py
+++ hamurabiv2.py
@@ -89,10 +89,12 @@ class Hamurabiv2(object):
             starved_folk_total += starved_folk
             peopleDied += starved_folk
             if starved_folk > 0 and Hamurabiv2.uprising_flag(population, starved_folk):
-                gameON = false
+                gameON = False
                 print("Too many of your people have gone hungry, Hammurabi.\n"
                       "You are no longer ruler.")
-            immigrant = Hamurabiv2.immigrants(population, acresOfLand, bushels_in_storage)
+                break
+            if starved_folk == 0:
+                immigrant = Hamurabiv2.immigrants(population, acresOfLand, bushels_in_storage)
             harvest_and_yield = Hamurabiv2.harvest(acres_planted)
             harvested_bushels = harvest_and_yield[0]
             cropyield = harvest_and_yield[1]
@@ -105,19 +107,19 @@ class Hamurabiv2(object):
             bushels -= rat_damage
             bushels += harvest_and_yield[0]
 
-        # EOY Result Screen / EOG Grade
-        print_result(year, starved_folk, immigrant, population, harvested_bushels,
+            # EOY Result Screen / EOG Grade
+            Hamurabiv2.print_result(year, starved_folk, immigrant, population, harvested_bushels,
                      cropyield, bushels, rat_damage, acresOfLand, landValue)
 
-        if year == 10:
-            final_result(total_people_starved, acresOfLand, population)
-            gameON = False
-        print(Hamurabiv2.plague_chance(population))
-        print(Hamurabiv2.starvation_deaths(population, bushels))
-        print(Hamurabiv2.uprising_flag(population, starved_folk))
-        print(Hamurabiv2.immigrants(population, acresOfLand, bushels_in_storage))
-        print(Hamurabiv2.grain_eaten_by_rats(bushels))
-        print(Hamurabiv2.new_cost_of_land())
+            if year == 10:
+                Hamurabiv2.final_result(total_people_starved, acresOfLand, population)
+                gameON = False
+        # print(Hamurabiv2.plague_chance(population))
+        # print(Hamurabiv2.starvation_deaths(population, bushels))
+        # print(Hamurabiv2.uprising_flag(population, starved_folk))
+        # print(Hamurabiv2.immigrants(population, acresOfLand, bushels_in_storage))
+        # print(Hamurabiv2.grain_eaten_by_rats(bushels))
+        # print(Hamurabiv2.new_cost_of_land())
 
     def summary():
         print("Congratulations, you are the newest ruler of ancient Sumer, elected for a ten year term of office.\n"
@@ -206,11 +208,11 @@ class Hamurabiv2(object):
         immigrant_total = int((20 * acresOfLand + bushels_in_storage) / (100 * population))
         return immigrant_total
 
-    def grain_eaten_by_rats(bushels):
+    def grain_eaten_by_rats(stored_bushels):
         chance = random.randint(1, 100)
         if chance < 40:
             percent_eaten = random.randint(10, 30)
-            return bushels // (percent_eaten / 100)
+            return stored_bushels // (percent_eaten // 100)
         else:
             return 0
 
@@ -223,35 +225,35 @@ class Hamurabiv2(object):
         return landValue
 
 
-def print_result(year, starved_people, immigrant_arrivals, population, harvest_bushels,
+    def print_result(year, starved_people, immigrant_arrivals, population, harvest_bushels,
                  bushels_per_acre, bushels_stored, rat_damage, acres_owned, land_value):
-    # figure out the strings to keep this code for the last year & just plug in the alternate strings?
-    # Would also be nice to be able to change time period / setting of the game.
-    # string_changes = (("You are in year", "This was year ten"),
-    #                   ("In the previous year", "Over the years"),
-    #                   ("The population is now", "Our great kingdom has grown from 100 proud citizens to"),
-    #                   ("Over your rule we harvested"
-    print("O great Hammurabi!")
-    print(f"You are in year {year} of your ten year rule")
-    print(f"In the previous year {starved_people} starved to death.")
-    print(f"In the previous year {immigrant_arrivals} entered the kingdom.")
-    print(f"The population is now {population}.")
-    print(f"We harvested {harvest_bushels} bushels at {bushels_per_acre}.")
-    print(f"Rats destroyed{rat_damage} bushels, leaving {bushels_stored} bushels in storage.")
-    print(f"The city owns {acres_owned} acres of land.")
-    print(f"Land is currently worth {land_value} per acre.")
+        # figure out the strings to keep this code for the last year & just plug in the alternate strings?
+        # Would also be nice to be able to change time period / setting of the game.
+        # string_changes = (("You are in year", "This was year ten"),
+        #                   ("In the previous year", "Over the years"),
+        #                   ("The population is now", "Our great kingdom has grown from 100 proud citizens to"),
+        #                   ("Over your rule we harvested"
+        print("O great Hammurabi!")
+        print(f"These are the results of year {year} of your ten year rule")
+        print(f"In the previous year {starved_people} starved to death.")
+        print(f"In the previous year {immigrant_arrivals} new citizen immigrated to the kingdom.")
+        print(f"The population is now {population}.")
+        print(f"We harvested {harvest_bushels} bushels at {bushels_per_acre} bushels per acre.")
+        print(f"Rats destroyed {rat_damage} bushels, leaving {bushels_stored} bushels in storage.")
+        print(f"The city owns {acres_owned} acres of land.")
+        print(f"Land is currently worth {land_value} per acre.")
 
 
-def final_result(total_people_starved, acres, end_population):
-    # started w/ 10 acres per person
-    acres_per_person = acres / end_population
-    if total_people_starved < 20 and acres_per_person > 12:
-        print("O Hammurabi! Our kingdom has never flourished so!")
-    elif total_people_starved < 50 and acres_per_person > 11:
-        print("Hammurabi, your reign was as we expected. Our kingdom continues on.")
-    else:
-        print("Your reign has ended, may your next endeavors be more to your abilities.")
-    print("Your reign has ended.")
+    def final_result(total_people_starved, acres, end_population):
+        # started w/ 10 acres per person
+        acres_per_person = acres / end_population
+        if total_people_starved < 20 and acres_per_person > 12:
+            print("O Hammurabi! Our kingdom has never flourished so!")
+        elif total_people_starved < 50 and acres_per_person > 11:
+            print("Hammurabi, your reign was as we expected. Our kingdom continues on.")
+        else:
+            print("Your reign has ended, may your next endeavors be more to your abilities.")
+        print("Your reign has ended.")
 
 
 if __name__ == '__main__':
